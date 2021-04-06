6754e806b9351ad9bd3c80be4cd5bf1427c77c24
Dormarble <gudfla0814@gmail.com>
2021-04-06 17:13:04 +0900

    [#11] 일반 로고 파일 수정

--- src/components/FillLogo.jsx
+++ src/components/FillLogo.jsx
@@ -4,10 +4,10 @@ import { useLogoStyles } from '@utils/styles/Logo';
 
 
 export default function FillLogo({size}) {
-    const logoClasses = useLogoStyles();
+    const logoClasses = useLogoStyles(size);
 
     return (
-        <Box className={logoClasses[size]}>
+        <Box className={logoClasses.container}>
             <Box className={logoClasses.firstBar}></Box>
             <Box className={logoClasses.secondBar}></Box>
             <Box className={logoClasses.thirdBar}></Box>
