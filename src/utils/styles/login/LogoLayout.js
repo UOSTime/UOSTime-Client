aec64913ab0a71b493e7dedd328fd390e5bb77d2
Dormarble <gudfla0814@gmail.com>
2021-04-06 23:24:17 +0900

    [#11] 로고 hover 동작 추가

--- src/utils/styles/login/LogoLayout.js
+++ src/utils/styles/login/LogoLayout.js
@@ -5,8 +5,8 @@ import { neumorphismForm } from '@utils/styles/Neumorphism';
 const useLogoLayoutStyles = makeStyles({
   root: {
     flexShrink: 0,
-    [theme.breakpoints.down('md')]: 
-      neumorphismForm.positive({
+    [theme.breakpoints.down('md')]: {
+      ...neumorphismForm.positive({
         marginTop: '20px',
         width: '165px',
         height: '165px', 
@@ -15,8 +15,18 @@ const useLogoLayoutStyles = makeStyles({
         blur: '15px', 
         direction: 'column'
       }),
-    [theme.breakpoints.between('md', 'lg')]: 
-      neumorphismForm.positive({
+      '&:hover': neumorphismForm.negative({
+        marginTop: '20px',
+        width: '165px',
+        height: '165px', 
+        radius: '40px', 
+        distance: '15px', 
+        blur: '15px', 
+        direction: 'column'
+      })
+    },
+    [theme.breakpoints.between('md', 'lg')]: {
+      ...neumorphismForm.positive({
         width: '300px',
         height: '300px', 
         radius: '80px', 
@@ -24,8 +34,25 @@ const useLogoLayoutStyles = makeStyles({
         blur: '30px', 
         direction: 'column'
       }),
-    [theme.breakpoints.up('lg')]: 
-      neumorphismForm.positive({
+      '&:hover': neumorphismForm.negative({
+        width: '300px',
+        height: '300px', 
+        radius: '80px', 
+        distance: '30px', 
+        blur: '30px', 
+        direction: 'column'
+      })
+    },
+    [theme.breakpoints.up('lg')]: {
+      ...neumorphismForm.positive({
+        width: '500px', 
+        height: '500px', 
+        radius: '100px', 
+        distance: '50px', 
+        blur: '50px', 
+        direction: 'column'
+      }),
+      '&:hover': neumorphismForm.negative({
         width: '500px', 
         height: '500px', 
         radius: '100px', 
@@ -33,6 +60,8 @@ const useLogoLayoutStyles = makeStyles({
         blur: '50px', 
         direction: 'column'
       })
+    }
+      
   }
 });
 
