2e321602d8f517697dfb68b79354d4f42e520a23
Dormarble <gudfla0814@gmail.com>
2021-04-09 14:41:33 +0900

    [#11] 로고 길이 수정

--- src/utils/styles/Logo.js
+++ src/utils/styles/Logo.js
@@ -10,32 +10,32 @@ const common = {
 
 const barStyle = {
     firstBar: {
-        width: '21.65%',
-        height: '73.77%',
+        width: '22%',
+        height: '75%',
         background: 'linear-gradient(164.04deg, #F68B7D 0%, #F7C978 100.61%, #A6C0FE 205.07%)',
         borderRadius:'500px',
         position: 'relative',
         top: '-13%'
     },
     secondBar: {
-        width: '21.65%',
-        height: '65.57%',
+        width: '22%',
+        height: '65%',
         background: 'linear-gradient(159.8deg, #F68B7D -77.35%, #F7C978 45.19%, #A6C0FE 172.42%)',
         borderRadius:'500px',
         position: 'relative',
         top: '17%'
     },
     thirdBar: {
-        width: '21.65%',
-        height: '53.28%',
+        width: '22%',
+        height: '55%',
         background: 'linear-gradient(154.42deg, #F68B7D -92.15%, #F7C978 35.1%, #A6C0FE 167.22%)',
         borderRadius:'500px',
         position: 'relative',
         top: '-13%'
     },
     fourthBar: {
-        width: '21.65%',
-        height: '40.98%',
+        width: '22%',
+        height: '40%',
         background: 'linear-gradient(148.11deg, #F68B7D -203.85%, #F7C978 -53.06%, #A6C0FE 103.49%)',
         borderRadius:'500px',
         position: 'relative',
