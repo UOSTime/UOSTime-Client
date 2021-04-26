c4dbd954358c026b6fd5a8290b7086d7b29b542c
donghoonKang-dev <donghoon.dev@gmail.com>
2021-04-26 20:04:04 +0900

    [#35] Add inactivated chat and notify button and user button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { makeStyles, useTheme, AppBar, Tabs, Tab, Typography, Box, Container } from '@material-ui/core';
+import { makeStyles, useTheme, AppBar, Tabs, Tab, Typography, Box, Link, Button, IconButton } from '@material-ui/core';
+import ForumIcon from '@material-ui/icons/Forum';
+import ForumOutlinedIcon from '@material-ui/icons/ForumOutlined';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import NotificationsNoneOutlinedIcon from '@material-ui/icons/NotificationsNoneOutlined';
+import PersonIcon from '@material-ui/icons/Person';
 import Logo from './Logo';
 
 function a11yProps(index) {
@@ -12,6 +17,7 @@ function a11yProps(index) {
 const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
+    margin: '0 0 0 0',
   },
   appBar: {
     backgroundColor: '#f6f6f6',
@@ -22,7 +28,6 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     flexDirection: 'row',
     color: '#4e4e4e',
-    flexGrow: 0.08,
     alignItems: 'center',
     padding: '6px 12px',
   },
@@ -42,6 +47,28 @@ const useStyles = makeStyles(theme => ({
   indicator: {
     height: '4px',
   },
+  buttonBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 'auto',
+    marginRight: '4px'
+  },
+  inactivedButton: {
+    color: '#C4C4C4',
+    fontSize: '28px',
+  },
+  activedButton: {
+
+  },
+  userButton: {
+    color: '#A6C0FE',
+    fontSize: '18px',
+    padding: '12px 12px 12px 24px',
+  },
+  innerIcon: {
+    transform: 'scale(1.4)',
+  },
 }));
 
 export default function Header() {
@@ -57,12 +84,14 @@ export default function Header() {
   };
 
   return (
-    <Container className={classes.root}>
+    <Box className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
-        <Box className={classes.logoBox}>
-          <Logo size='smd'/>
-          <Typography className={classes.font}>UOSTime</Typography>
-        </Box>
+        <Link href='/' underline='none'>
+          <Box className={classes.logoBox}>
+            <Logo size='smd'/>
+            <Typography className={classes.font}>UOSTime</Typography>
+          </Box>
+        </Link>
         <Tabs
           value={value}
           onChange={handleChange}
@@ -74,7 +103,17 @@ export default function Header() {
           <Tab label="게시판" classes={{ root: classes.tab, selected: classes.selected }} {...a11yProps(1)} />
           <Tab label="공지사항" classes={{ root: classes.tab, selected: classes.selected }} {...a11yProps(2)} />
         </Tabs>
+        <Box className={classes.buttonBox}>
+          <IconButton aria-label="chat"><ForumOutlinedIcon className={classes.inactiveButton} /></IconButton>
+          <IconButton aria-label="notification"><NotificationsNoneOutlinedIcon className={classes.inactiveButton} /></IconButton>
+          <Button
+            className={classes.userButton}
+            startIcon={<PersonIcon className={classes.innerIcon} />}
+          >
+            ThisIsUser
+          </Button>
+        </Box>
       </AppBar>
-    </Container>
+    </Box>
   );
 }
