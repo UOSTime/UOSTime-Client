77e2d21bf540a580e379f5259a5dd32bba3e5a16
dewy.i <dewy.i@kakaocorp.com>
2021-04-29 02:03:13 +0900

    [#20] Update use of requestAPI()

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,32 +5,27 @@ import { useShowPopup } from '@components/Popup';
 
 const { API_URL_BASE } = process.env;
 
-const makeAPI = (method, path) => (initialBody = {}) => {
-  const API = {
-    ...method(path),
-    data: initialBody,
+const makeConfig = ({ method, url }) => (initialData = {}) => {
+  const config = {
+    method,
+    url,
 
-    setPathParam: (...pathParams) => {
-      API.url += ['', ...pathParams].join('/');
-      return API;
+    setPath: (...path) => {
+      config.url += ['', ...path].join('/');
+      return config;
     },
 
-    setQuery: queries => {
-      const queryStr = Object.entries(queries)
-        .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
-        .join('&');
-
-      API.url = `${API.url}?${queryStr}`;
-      return API;
-    },
-
-    setBody: body => {
-      API.data = body;
-      return API;
+    setData: data => {
+      if (method === 'GET') {
+        config.params = data;
+      } else {
+        config.data = data;
+      }
+      return config;
     },
   };
 
-  return API;
+  return config.setData(initialData);
 };
 
 // API CONFIG OBJECT
@@ -41,27 +36,21 @@ const PATCH = url => ({ method: 'PATCH', url });
 const DELETE = url => ({ method: 'DELETE', url });
 
 // API CONFIG LIST
-
-export const API_LOGIN = makeAPI(POST, '/user/login');
-export const API_SIGN_UP = makeAPI(POST, '/user');
-export const API_FIND_ID = makeAPI(GET, '/user/id');
-export const API_FIND_PW = makeAPI(GET, '/user/password');
-export const API_GET_SEMESTERS = makeAPI(GET, '/semesters');
-export const API_GET_ALL_NOTICES = makeAPI(GET, '/notice/all');
-export const API_CREATE_NOTICE = makeAPI(POST, '/notice');
-export const API_UPDATE_NOTICE = makeAPI(PATCH, '/notice');
-export const API_DELETE_NOTICE = makeAPI(DELETE, '/notice');
-export const API_GET_ALL_LECTURES = makeAPI(GET, '/lecture');
-export const API_CREATE_TIMETABLE = makeAPI(POST, '/timetable');
-export const API_DELETE_TIMETABLE = makeAPI(DELETE, '/timetable');
-export const API_PATCH_TIMETABLE_NAME = makeAPI(PATCH, '/timetable/name');
-export const API_GET_TIMETABLES = makeAPI(GET, '/timetable');
-export const API_DELETE_TLECTURE = makeAPI(DELETE, '/timetable/tlecture');
-export const API_ADD_TLECTURE = makeAPI(POST, '/timetable/tlecture');
-export const API_UPDATE_LECTURES = makeAPI(PATCH, '/lecture');
-export const API_GET_HISTORIES = makeAPI(GET, '/history');
-export const API_UPDATE_LECTURES = makeAPI(PATCH, '/lecture');
-export const API_GET_HISTORIES = makeAPI(GET, '/history');
+export const API_LOGIN = makeConfig(POST('/user/login'));
+export const API_GET_SEMESTERS = makeConfig(GET('/semesters'));
+export const API_GET_ALL_NOTICES = makeConfig(GET('/notice/all'));
+export const API_CREATE_NOTICE = makeConfig(POST('/notice'));
+export const API_UPDATE_NOTICE = makeConfig(PATCH('/notice'));
+export const API_DELETE_NOTICE = makeConfig(DELETE('/notice'));
+export const API_GET_ALL_LECTURES = makeConfig(GET('/lecture'));
+export const API_CREATE_TIMETABLE = makeConfig(POST('/timetable'));
+export const API_DELETE_TIMETABLE = makeConfig(DELETE('/timetable'));
+export const API_PATCH_TIMETABLE_NAME = makeConfig(PATCH('/timetable/name'));
+export const API_GET_TIMETABLES = makeConfig(GET('/timetable'));
+export const API_DELETE_TLECTURE = makeConfig(DELETE('/timetable/tlecture'));
+export const API_ADD_TLECTURE = makeConfig(POST('/timetable/tlecture'));
+export const API_UPDATE_LECTURES = makeConfig(PATCH('/lecture'));
+export const API_GET_HISTORIES = makeConfig(GET('/history'));
 
 const axiosInstance = axios.create({
   baseURL: `${API_URL_BASE}/api`,
@@ -85,13 +74,7 @@ axiosInstance.interceptors.response.use(
   error => Promise.resolve(error.response),
 );
 
-export async function requestAPI(apiConfig, data) {
-  const config = {
-    url: apiConfig.url,
-    method: apiConfig.method,
-    data: data || apiConfig.data,
-  };
-
+export async function requestAPI(config) {
   try {
     const response = await axiosInstance.request(config);
 
