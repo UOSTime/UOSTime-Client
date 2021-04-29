8cc36d87c00eb0bf1fb40d7bfe8499396aa4fe25
Dormarble <gudfla0814@gmail.com>
2021-04-29 21:25:42 +0900

    [#54] 들여쓰기 수정

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -5,19 +5,18 @@ let socket;
 export function getSocket() {
     if(!socket) {
       socket = io(process.env.API_URL_BASE, {
-          path: '/websocket/entryPoint',
-          transports: ['websocket'],
-          query: {
-            token: window.localStorage.getItem('token')
-          },
-          reconnectionAttempts: 10
-        });
+        path: '/websocket/entryPoint',
+        transports: ['websocket'],
+        query: {
+          token: window.localStorage.getItem('token')
+        },
+        reconnectionAttempts: 10
+      });
 
-      
       socket.on('connect', () => {
-          console.log('connected');
+        console.log('connected');
       });
     }
-
+      
     return socket;
 }
