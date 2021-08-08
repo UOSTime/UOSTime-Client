0df48429fea5cee461575141f8e6a35587012411
dewy.i <dewy.i@kakaocorp.com>
2021-08-09 00:21:38 +0900

    [#66] 로그인 화면 표시 개선

--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -1,13 +1,12 @@
 import StatusCodes from 'http-status-codes';
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 import { Button, Box, makeStyles, Container, Typography, Link } from '@material-ui/core';
 import Loading from '@components/Loading';
 import UosInput from '@components/UosInput';
-import { userIDState } from '@states/User';
 import { semesterState } from '@states/Semester';
-import { API_LOGIN, API_GET_SEMESTER, requestAPI, removeToken, setToken } from '@utils/api';
+import { API_LOGIN, API_GET_SEMESTER, requestAPI, getToken, setToken } from '@utils/api';
 import { foregroundColor } from '@utils/styles/Colors';
 import useLogoLayoutStyles from '@utils/styles/login/LogoLayout';
 import useLoginLabelStyles from '@utils/styles/login/LoginLabel';
@@ -20,13 +19,21 @@ import FindPWDialog from '@views/login/FindPw';
 import Logo from '@views/login/Logo';
 import SignUpDialog from './SignUp';
 
+const onChange = setter => e => setter(e.target.value);
+
 export default function Login() {
-  const [userID, setUserID] = useRecoilState(userIDState);
+  if (getToken()) {
+    return <Redirect to="/" />;
+  }
+  return <LoginBox />;
+}
+
+export function LoginBox() {
   const setSemester = useSetRecoilState(semesterState);
-  const [loginInfo, setLoginInfo] = useState({ uid: '', pw: '' });
+  const [uid, setUid] = useState('');
+  const [pw, setPw] = useState('');
   const [loading, setLoading] = useState(false);
   const [warning, setWarning] = useState('');
-  const [error, setError] = useState(null);
   const [findOpen, setFindOpen] = useState({
     id: false,
     pw: false,
@@ -39,8 +46,6 @@ export default function Login() {
   const linkClass = useLinkStyles();
   const buttonClass = useButtonStyles();
 
-  const loginBtn = useRef();
-
   const callSemester = async () => {
     const response = await requestAPI(API_GET_SEMESTER());
 
@@ -51,52 +56,39 @@ export default function Login() {
     setSemester('semester', semester);
   };
 
-  const login = () => {
-    if (!(loginInfo.uid.length && loginInfo.pw.length)) {
+  const login = async () => {
+    if (!uid || !pw) {
       setWarning('아이디와 비밀번호를 입력해주세요!');
       return;
     }
 
-    const callLogin = async () => {
-      setLoading(true);
-      setError(null);
+    setLoading(true);
+    const response = await requestAPI(API_LOGIN({ uid, pw }));
+    setLoading(false);
 
-      removeToken();
+    if (response.status !== StatusCodes.OK) {
+      switch (response.status) {
+        case StatusCodes.UNAUTHORIZED:
+          setWarning('아이디 혹은 비밀번호가 틀렸어요!');
+          setPw('');
+          break;
 
-      const response = await requestAPI(API_LOGIN(loginInfo));
+        case StatusCodes.BAD_REQUEST:
+          setWarning('클라이언트에서 오류가 났어요. 계속 시도해도 안되면 UOSTime 팀에게 문의해주세요!');
+          break;
 
-      if (response.status !== StatusCodes.OK) {
-        switch (response.status) {
-          case StatusCodes.UNAUTHORIZED:
-            setWarning('아이디 혹은 비밀번호가 틀렸어요!');
-            setLoginInfo({
-              ...loginInfo,
-              pw: '',
-            });
-            break;
-
-          case StatusCodes.BAD_REQUEST:
-            setWarning('클라이언트에서 오류가 났어요. 계속 시도해도 안되면 UOSTime 팀에게 문의해주세요!');
-            break;
-
-          default:
-            setWarning('서버에서 오류가 났어요. 계속 시도해도 안되면 UOSTime 팀에게 문의해주세요!');
-            break;
-        }
-        setLoading(false);
-        return;
+        default:
+          setWarning('서버에서 오류가 났어요. 계속 시도해도 안되면 UOSTime 팀에게 문의해주세요!');
+          break;
       }
-
-      setToken(response.data.token);
-      window.localStorage.setItem('userID', response.data.userId);
-      setUserID(response.data.userId);
-
-      await callSemester();
-      getSocket();
-
       setLoading(false);
-    };
-    callLogin();
+      return;
+    }
+
+    setToken(response.data.token);
+    await callSemester();
+    getSocket();
+    setLoading(false);
   };
 
   const dialogOnClose = () => {
@@ -123,25 +115,19 @@ export default function Login() {
     });
   };
 
-  const inputOnChange = e => {
-    const { value, name } = e.target;
-    setLoginInfo({
-      ...loginInfo,
-      [name]: value,
-    });
-  };
-
   const onEnterPress = e => {
     if (e.key === 'Enter') {
       login();
     }
   };
 
-  if (userID) {
-    getSocket();
-    return <Redirect path="/" />;
+  if (getToken()) {
+    // refresh page
+    // TODO: refrech using state on App.js
+    window.location.reload();
+    return null;
   }
-  if (error) return <Box>에러가 발생했어요...</Box>;
+
   return (
     <Container className={classes.container}>
       <Box className={logoLayoutClass.root}>
@@ -154,16 +140,16 @@ export default function Login() {
             type="text"
             name="uid"
             label="아이디"
-            onChange={inputOnChange}
-            value={loginInfo.uid}
+            onChange={onChange(setUid)}
+            value={uid}
           />
           <UosInput
             type="password"
             name="pw"
             label="비밀번호"
-            onChange={inputOnChange}
+            onChange={onChange(setPw)}
             onKeyPress={onEnterPress}
-            value={loginInfo.pw}
+            value={pw}
           />
           {warning.length > 0 ? (
             <Typography className={classes.warning}>{warning}</Typography>
@@ -172,7 +158,6 @@ export default function Login() {
         <Button
           className={buttonClass.linearRed}
           onClick={login}
-          ref={loginBtn}
         >
           Login
         </Button>
