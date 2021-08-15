1e939bd5c3905849d7172a838d2b84edcd2bebec
dewy.i <dewy.i@kakaocorp.com>
2021-08-15 22:48:18 +0900

    [#46] 현재 학기 설정 및 표시

--- src/utils/semester.js
+++ src/utils/semester.js
@@ -1,16 +1,28 @@
 const Terms = {
-  A10: '1학기',
-  A11: '여름계절학기',
-  A20: '2학기',
-  A21: '겨울계절학기',
+  A10: {
+    name: '1학기',
+    month: 1,
+  },
+  A11: {
+    name: '여름계절학기',
+    month: 5,
+  },
+  A20: {
+    name: '2학기',
+    month: 7,
+  },
+  A21: {
+    name: '겨울계절학기',
+    month: 11,
+  },
 };
 
-export function getAllTerms() {
-  return Object.entries(Terms).map(([termCode, termName]) => ({ termCode, termName }));
-}
+export const getTermsAsArray = () => Object
+  .entries(Terms)
+  .map(([code, { name, month }]) => ({
+    code,
+    name,
+    month,
+  }));
 
-export function getTermNamebyCode(code) {
-  const termName = Terms[code];
-  if (termName) return termName;
-  throw new Error(`Unexpected term code ${code}.`);
-}
+export const getTermNamebyCode = code => Terms[code]?.name;
--- src/views/timetable/SearchBar.jsx
+++ src/views/timetable/SearchBar.jsx
@@ -8,44 +8,77 @@ import {
   MenuItem,
   FormControl,
   TextField,
+  Divider,
 } from '@material-ui/core';
 import { searchLectureListState } from '@states/Lecture';
 import { requestAPI, API_GET_ALL_LECTURES } from '@utils/api';
+import { getTermsAsArray, getTermNamebyCode } from '@utils/semester';
+import { getToday } from '@utils/time';
 
-const searchOption = {
-  sub_dept: '학부(과)',
-  subject_div: '과목구분',
-  subject_div2: '세부과목',
-  subject_no: '과목번호',
-  subject_nm: '과목명',
-  shyr: '학년',
-  credit: '학점',
-  prof_nm: '교수명',
+const setValue = setter => e => setter(e.target.value);
+
+const getSemesterValue = (year, term) => `${year}-${term}`;
+
+const getSemesterOptions = () => {
+  const terms = getTermsAsArray().reverse();
+  const { year, month } = getToday();
+
+  const semesterOptions = [];
+
+  for (let i = 0; i < 4; i++) {
+    terms.forEach(term => {
+      if (i || month >= term.month) {
+        semesterOptions.push((
+          <MenuItem
+            key={getSemesterValue(year - i, term.code)}
+            value={getSemesterValue(year - i, term.code)}
+          >
+            {`${year - i}년 ${getTermNamebyCode(term.code)}`}
+          </MenuItem>
+        ));
+      }
+    });
+    semesterOptions.push(<Divider key={i} />);
+  }
+
+  semesterOptions.pop();
+  return semesterOptions;
+};
+
+const getSearchTypeOptions = () => {
+  const searchOption = {
+    sub_dept: '학부(과)',
+    subject_div: '과목구분',
+    subject_div2: '세부과목',
+    subject_no: '과목번호',
+    subject_nm: '과목명',
+    shyr: '학년',
+    credit: '학점',
+    prof_nm: '교수명',
+  };
+  return Object.entries(searchOption).map(([code, name]) => (
+    <MenuItem key={name} value={code}>{name}</MenuItem>
+  ));
 };
 
 export default function SearchBar() {
   const setSearchLectureListState = useSetRecoilState(searchLectureListState);
+
+  const [selectedSemester, setSelectedSemester] = useState(getSemesterValue(2021, 'A10')); // TODO: set default value
   const [searchType, setSearchType] = useState('subject_nm');
   const [keyword, setKeyword] = useState('');
 
   const classes = useStyles();
 
-  const onSelectSearchType = e => {
-    setSearchType(e.target.value);
-  };
-
-  const onTypeKeyword = e => {
-    setKeyword(e.target.value);
-  };
-
   useEffect(() => {
     onSearch();
-  }, [searchType, keyword]);
+  }, [selectedSemester, searchType, keyword]);
 
   const onSearch = async () => {
+    const [year, term] = selectedSemester.split('-');
     const query = {
-      year: 2021,
-      term: 'A10',
+      year,
+      term,
       searchType,
       keyword: keyword.trim(),
     };
@@ -71,31 +104,47 @@ export default function SearchBar() {
 
   return (
     <Container className={classes.root}>
-      <FormControl className={classes.select} size="small">
-        <Select value={searchType} onChange={onSelectSearchType} variant="outlined">
-          {Object.entries(searchOption).map(([code, name]) => (
-            <MenuItem key={name} value={code}>{name}</MenuItem>
-          ))}
+      <FormControl size="small">
+        <Select value={selectedSemester} onChange={setValue(setSelectedSemester)} variant="outlined">
+          {getSemesterOptions()}
+        </Select>
+      </FormControl>
+      <FormControl size="small">
+        <Select value={searchType} onChange={setValue(setSearchType)} variant="outlined">
+          {getSearchTypeOptions()}
         </Select>
       </FormControl>
       <FormControl className={classes.input}>
-        <TextField onChange={onTypeKeyword} value={keyword} variant="outlined" size="small" />
+        <TextField onChange={setValue(setKeyword)} value={keyword} variant="outlined" size="small" />
       </FormControl>
     </Container>
   );
 }
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
-    margin: '0.5em 0',
-    padding: '0',
-  },
-  select: {
-    flexGrow: '1',
+    display: 'grid',
+    gap: '1em',
+    margin: '0',
+    padding: '1em 0',
+    [theme.breakpoints.down('lg')]: {
+      width: '100%',
+      gridTemplateColumns: '1fr 1fr',
+    },
+    [theme.breakpoints.up('lg')]: {
+      flexGrow: '2',
+      width: 'auto',
+      gridTemplateColumns: '1fr 1fr 2fr',
+    },
   },
   input: {
-    flexGrow: '2',
-    marginLeft: '1em',
+    [theme.breakpoints.down('lg')]: {
+      gridColumn: '1 / 3',
+      gridRow: '2',
+    },
+    [theme.breakpoints.up('lg')]: {
+      gridColumn: '3',
+      gridRow: '1',
+    },
   },
-});
+}));
