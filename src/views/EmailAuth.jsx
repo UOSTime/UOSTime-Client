e4177eb7ce16ef31706aba2e2ddfa56de5366194
GyuZzang <tndud0606@gmail.com>
2021-11-23 21:38:17 +0900

    feat(emailAuth): 이메일 인증 코드 api 수정 적용(userID), css 추가

--- src/views/EmailAuth.jsx
+++ src/views/EmailAuth.jsx
@@ -4,7 +4,6 @@ import {
   requestAPI,
   API_MAKE_AUTH_CODE,
   API_MATCH_AUTH_CODE,
-  API_MY_INFO,
 } from "@utils/api";
 import {
   Button,
@@ -19,6 +18,8 @@ export default function EmailAuth() {
   const [userEmail, setUserEmail] = useState(null);
   const [isCodeSend, setIsCodeSend] = useState(false);
   const [code, setCode] = useState(null);
+
+  const classes = useStyles();
 
   const codeOnChange = (e) => {
     setCode(e.target.value);
@@ -41,7 +42,6 @@ export default function EmailAuth() {
   };
 
   const handleSendCode = async () => {
-    console.log(userEmail);
     try {
       const res = await requestAPI(API_MAKE_AUTH_CODE({ email: userEmail }));
       if (res.status === StatusCodes.NO_CONTENT) setIsCodeSend(true);
@@ -53,7 +53,8 @@ export default function EmailAuth() {
 
   const handleAuth = async () => {
     try {
-      const res = await requestAPI(API_MATCH_AUTH_CODE().setPath(code));
+      const userId = window.localStorage.getItem("userID");
+      const res = await requestAPI(API_MATCH_AUTH_CODE().setPath(userId, code));
       if (res.status === StatusCodes.NO_CONTENT) {
         alert("이메일이 성공적으로 인증되었습니다!");
       }
@@ -64,25 +65,31 @@ export default function EmailAuth() {
   };
 
   return (
-    <Container>
-      <Container>
-        <Typography>이메일 인증</Typography>
-        <Container>
-          <UosInput
-            type="email"
-            name="email"
-            label="email"
-            onChange={emailOnChange}
-            onKeyPress={emailOnEnterPress}
-            value={userEmail}
-          />
-          <Button onClick={handleSendCode}>인증 코드 전송</Button>
-        </Container>
+    <Container className={classes.container}>
+      <Typography className={classes.title}>이메일 인증</Typography>
+      <Typography className={classes.subTitle}>
+        서울시립대학교 포털 이메일 인증을 하신 후 강의 교환 서비스를 이용하실 수
+        있습니다.
+      </Typography>
+      <Container className={classes.inputWrapper}>
+        <UosInput
+          className={classes.input}
+          type="email"
+          name="email"
+          label="email"
+          onChange={emailOnChange}
+          onKeyPress={emailOnEnterPress}
+          value={userEmail}
+          placeholder="서울시립대학교 이메일을 입력해주세요."
+        />
+        <Button className={classes.button} onClick={handleSendCode}>
+          인증 코드 전송
+        </Button>
       </Container>
-
       {isCodeSend && (
-        <Container>
+        <Container className={classes.inputWrapper}>
           <UosInput
+            className={classes.input}
             type="password"
             name="code"
             label="인증코드"
@@ -90,9 +97,49 @@ export default function EmailAuth() {
             onKeyPress={codeOnEnterPress}
             value={code}
           />
-          <Button onClick={handleAuth}>인증</Button>
+          <Button className={classes.button} onClick={handleAuth}>
+            인증
+          </Button>
         </Container>
       )}
     </Container>
   );
 }
+
+const useStyles = makeStyles({
+  container: {
+    margin: "auto",
+  },
+  title: {
+    alignItems: "center",
+    textAlign: "center",
+    fontSize: "1.5rem",
+    fontWeight: "700",
+    marginBottom: "1rem",
+  },
+  subTitle: {
+    alignItems: "center",
+    textAlign: "center",
+    fontSize: "1rem",
+    marginButtom: "1rem",
+    color: "#A3A2A2",
+  },
+  inputWrapper: {
+    display: "flex",
+    width: "60%",
+    flexDirection: "row",
+    marginTop: "2rem",
+  },
+  input: {
+    flex: 9,
+  },
+  button: {
+    flex: 3,
+    marginLeft: "1rem",
+    backgroundColor: "#CFCFCF",
+    textAlign: "center",
+    boxShadow: "3px 4px 4px rgba(0, 0, 0, 0.25)",
+    borderRadius: "1.5rem",
+    color: "#FFF",
+  },
+});
